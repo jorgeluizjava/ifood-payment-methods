674108092109c86c70b2672cd49e2b17b56765d7
JORGE GALVAO <jorgegalvao@macjorge.local>
2020-04-29 07:23:05 -0300

    Criação do projeto

---
+++ src/main/java/br/com/challenge/ifoodpaymentmethods/IfoodPaymentMethodsApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.ifoodpaymentmethods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodPaymentMethodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodPaymentMethodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/challenge/ifoodpaymentmethods/IfoodPaymentMethodsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.ifoodpaymentmethods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodPaymentMethodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
